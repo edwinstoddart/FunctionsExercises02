253546699e3b346f31de30265e156d07170eb568
Edwin Stoddart <85506195+edwinstoddart@users.noreply.github.com>
2021-06-23 17:47:08 +0000

    Completed!!

--- index.js
+++ index.js
@@ -4,15 +4,15 @@
 //The function should return the area, NOT print it.
 
 //If only one argument is passed to the function, then the shape is a square. Modify your code to deal with this case.
-function makeRectangle(width, height = width) {
+function areaOfRectangle(width, height = width) {
   return width * height;
 }
 
 //Call your area function, then use a template literal to print, “The area is ____ cm^2.”
-let square = makeRectangle(4);
-let rectangle = makeRectangle(8, 4);
+let square = areaOfRectangle(4);
+let rectangle = areaOfRectangle(6, 4);
 
 console.log(`When passed one argument (4):
 The area is ${square} cm^2.`);
-console.log(`When passed two arguments (8, 4):
+console.log(`When passed two arguments (6, 4):
 The area is ${rectangle} cm^2.`);
